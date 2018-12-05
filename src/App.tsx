852408c7a5070093d6e228d5975bc79847375e66
Risky Feryansyah Pribadi <=>
2018-12-06 00:59:17 +0700

    App Layouting for User and Dosen Page

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+
+// import component
+import MiniDrawer from './components/Dashboard/MiniDrawer'
+// import Content from './components/Content/Content'
+// import TabMenu from './components/SignIn/TabMenu'
+
+// import cssbaseline
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createStyles, withStyles } from '@material-ui/core';
+
+
+
+const styles = createStyles({
+    root : {
+        display : 'flex',
+    }
+})
+
+interface IProps {
+  classes : any
+}
+
+class App extends React.Component<IProps> {
+  public render() {
+    const { classes } = this.props
+    return (
+        <div className={classes.root}>
+          <CssBaseline />
+          {/* <TabMenu /> */}
+          <MiniDrawer />
+          { this.props.children }
+          {/* <Content /> */}
+        </div>
+    );
+  }
+}
+
+export default withStyles(styles)(App)
