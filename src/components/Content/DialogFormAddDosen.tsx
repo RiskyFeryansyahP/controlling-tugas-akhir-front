0525a59260b172c37965d1a78f8f9e460cb3266b
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2018-12-13 00:30:40 +0700

    create Mutation in DialogForm to add dosen for Mahasiswa

--- src/components/Content/DialogFormAddDosen.tsx
+++ src/components/Content/DialogFormAddDosen.tsx
@@ -1,4 +1,8 @@
 import * as React from 'react'
+import { Mutation } from 'react-apollo'
+
+// import query Mutation
+import { AddDosenToMahasiswaMutation} from '../../queries/Query'
 
 // import @material-ui/core
 import Button from '@material-ui/core/Button'
@@ -10,33 +14,47 @@ import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 
 const DialogForm = (props : any) => {
+    console.log(props.code)
     return(
-        <React.Fragment>
-            <Dialog
-                open={props.open}
-                onClose={props.handleClose}
-                aria-labelledby="Form Add Dosen"
-            >
-                <DialogTitle> TAMBAHKAN DOSEN PEMBIMBING </DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Silahkan tambahkan dosen pembimbing anda dengan menggunakan code yang telah diberikan oleh dosen anda
-                    </DialogContentText>
-                    <TextField 
-                        autoFocus={true}
-                        margin='dense'
-                        id='code'
-                        name='code'
-                        label='Code Dosen'
-                        type='text'
-                        fullWidth={true}
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={props.handleClose}> TAMBAHKAN </Button>
-                </DialogActions>
-            </Dialog>
-        </React.Fragment>
+        <Mutation
+            mutation={AddDosenToMahasiswaMutation}
+        >
+            {(addDosenToMahasiswa) => {
+                const onSubmitDosenToMahasiswa = (e : any) => props.submitCodeDosen(e, addDosenToMahasiswa)
+                return(
+                    <React.Fragment>
+                                <Dialog
+                                    open={props.open}
+                                    onClose={props.handleClose}
+                                    aria-labelledby="Form Add Dosen"
+                                >
+                                    <DialogTitle> TAMBAHKAN DOSEN PEMBIMBING </DialogTitle>
+                                    <DialogContent>
+                                        <DialogContentText>
+                                            Silahkan tambahkan dosen pembimbing anda dengan menggunakan code yang telah diberikan oleh dosen anda
+                                        </DialogContentText>
+                                        <TextField 
+                                            autoFocus={true}
+                                            margin='dense'
+                                            id='code'
+                                            name='code'
+                                            label='Code Dosen'
+                                            type='text'
+                                            value={props.code}
+                                            fullWidth={true}
+                                            onChange={props.handleChange}
+                                        />
+                                    </DialogContent>
+                                    <DialogActions>
+                                        <form onSubmit={onSubmitDosenToMahasiswa}>
+                                            <Button type='submit'> TAMBAHKAN </Button>
+                                        </form>
+                                    </DialogActions>
+                                </Dialog>
+                        </React.Fragment>
+                )
+            }}
+        </Mutation>
     )
 }
 
