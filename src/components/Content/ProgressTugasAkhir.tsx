0ccce591a268d5f8ca81c5a2c365bd24acda2c3b
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2018-12-20 22:02:32 +0700

    added feaute auto checklist progress when you done it

--- src/components/Content/ProgressTugasAkhir.tsx
+++ src/components/Content/ProgressTugasAkhir.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react'
+// import { Query } from 'react-apollo'
+
+// import { getMahasiswaQuery } from '../../queries/Query'
 
 // import @material-ui/core component
 import Stepper from '@material-ui/core/Stepper'
 import Step from '@material-ui/core/Step'
-import { StepButton } from '@material-ui/core';
+// import { StepButton } from '@material-ui/core';
+import StepLabel from '@material-ui/core/StepLabel'
 import { withStyles, createStyles } from '@material-ui/core/styles'
 
 const styles = (theme : any) => createStyles({
@@ -24,6 +28,8 @@ const styles = (theme : any) => createStyles({
 
 interface IProps {
     classes : any
+    id_mahasiswa : string
+    tugas : any
 }
 
 interface IState {
@@ -40,22 +46,32 @@ class ProgressTugasAkhir extends React.Component<IProps, IState> {
         }
     }
 
+    public componentWillMount()
+    {
+        if(this.props.tugas.bab1 === 'Done')
+        {
+            this.setState({ activeStep : this.state.activeStep+1 })
+        }
+    }
+
     public render()
     {
+        // console.log("id_mahasiswa2 ", this.props.id_mahasiswa)
         const { classes } = this.props
         const { activeStep } = this.state
         const steps = ['BAB 1', 'BAB 2', 'BAB 3', 'BAB 4', 'BAB 5']
         return(
             <div className={classes.root}>
-                <Stepper nonLinear={true} activeStep={activeStep}>
+                <Stepper activeStep={activeStep} alternativeLabel={true}>
                     {steps.map((label, index) => {
                         return(
                             <Step key={label}>
-                                <StepButton
+                                {/* <StepButton
                                     completed={this.state.completed[index]}
                                 >
                                     {label}
-                                </StepButton>
+                                </StepButton> */}
+                                 <StepLabel>{label}</StepLabel>
                             </Step>
                         )
                     })}
