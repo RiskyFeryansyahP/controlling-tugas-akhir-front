82529dcab750b8b87a044a931ec5023f51b3b087
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2018-12-20 22:03:32 +0700

    create DialogFormAddMeet to make meet with dosen pembimbing

---
+++ src/components/Meeting/DialogFormAddMeet.tsx
@@ -0,0 +1,103 @@
+import * as React from 'react'
+import { Query, Mutation } from 'react-apollo'
+
+// import query graphql
+import { getMahasiswaQuery, MahasiswaAddMeetWithDosenMutation } from '../../queries/Query'
+
+// import component @material-ui/core
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+
+
+const DialogFormAddMeet = (props : any) => {
+    console.log('props id ', props.id_mahasiswa)
+    return(
+        <React.Fragment>
+            <Query
+                query={getMahasiswaQuery}
+                variables={{ id :  props.id_mahasiswa}}
+            >
+                {({ data, loading }) => {
+                    console.log(data)
+                    return(
+                        <Dialog
+                            open={props.open}
+                            onClose={props.handleClose}
+                            aria-labelledby="Form Add Dosen"
+                        >
+                            <DialogTitle> BUAT JADWAL BERTEMU DENGAN DOSEN </DialogTitle>
+                            <DialogContent>
+                                <DialogContentText>
+                                    Silahkan membuat jadwal janji bertemu anda untuk konsul dengan dosen
+                                </DialogContentText>
+                                <TextField 
+                                    autoFocus={true}
+                                    margin='dense'
+                                    id='keterangan'
+                                    name='keterangan'
+                                    label='Keterangan'
+                                    type='text'
+                                    value={props.keterangan}
+                                    fullWidth={true}
+                                    onChange={props.handleChange}
+                                />
+                                <TextField 
+                                    margin='dense'
+                                    id='jam_awal'
+                                    name='jam_awal'
+                                    label='Jam Awal'
+                                    placeholder='contoh : 08:00'
+                                    type='text'
+                                    value={props.jam_awal}
+                                    fullWidth={true}
+                                    onChange={props.handleChange}
+                                />
+                                <TextField 
+                                    margin='dense'
+                                    id='jam_akhir'
+                                    name='jam_akhir'
+                                    label='Jam Akhir'
+                                    placeholder='contoh : 10:00'
+                                    type='text'
+                                    value={props.jam_akhir}
+                                    fullWidth={true}
+                                    onChange={props.handleChange}
+                                />
+                                <TextField 
+                                    margin='dense'
+                                    id='dosen'
+                                    name='dosen_code'
+                                    label='Kode Dosen'
+                                    disabled={true}
+                                    type='text'
+                                    value={data.mahasiswa.dosen.code}
+                                    fullWidth={true}
+                                    onChange={props.handleChange}
+                                />
+                            </DialogContent>
+                            <DialogActions>
+                                <Mutation
+                                    mutation={MahasiswaAddMeetWithDosenMutation}
+                                >
+                                    {(mahasiswaAddMeetwithDosen) => {
+                                        const submitMeetDosen = () => props.onSubmitMeetDosen(mahasiswaAddMeetwithDosen)
+                                        return(
+                                            <Button type='submit' onClick={submitMeetDosen}> TAMBAHKAN </Button>
+                                        )
+                                    }}
+                                </Mutation>
+                            </DialogActions>
+                        </Dialog>
+                    )
+                }}
+            </Query>
+        </React.Fragment>
+    )
+}
+
+export default DialogFormAddMeet
